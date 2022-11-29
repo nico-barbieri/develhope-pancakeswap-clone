c18b926575afedf9e43c040d1ea58d01400cff77
Nico Barbieri <nicobarbieri.m@gmail.com>
2022-11-29 20:35:03 +0100

    add animations: moving-cryptos, moving-graphs, cake-and-pancakes + update style-guide layout

--- script/animations.js
+++ script/animations.js
@@ -10,8 +10,9 @@ layers.forEach(layer => {
 function parallax(e){
     layers.forEach(layer => {
         const order = layer.getAttribute('data-layer');
-        const xMovement = - Math.ceil(e.clientX * order / 200);
+        const xMovement = - Math.ceil(e.clientX * order / 300);
         const yMovement = - Math.ceil(e.clientY * order / 300);
+        //changed top & left instead of transform to not conflict with animation
         layer.style.left = xMovement + 'px';
         layer.style.top = yMovement + 'px';
     })
