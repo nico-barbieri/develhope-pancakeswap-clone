094813dd43074aba08c9de3ce40ffdbe0051dcec
Nico Barbieri <nicobarbieri.m@gmail.com>
2022-11-30 19:31:56 +0100

    add animation in index, update index layout, modify animations.js

--- script/animations.js
+++ script/animations.js
@@ -10,10 +10,10 @@ layers.forEach(layer => {
 function parallax(e){
     layers.forEach(layer => {
         const order = layer.getAttribute('data-layer');
-        const xMovement = - Math.ceil(e.clientX * order / 300);
-        const yMovement = - Math.ceil(e.clientY * order / 300);
+        const xMovement = - (e.clientX * order / 600);
+        const yMovement = - (e.clientY * order / 600);
         //changed top & left instead of transform to not conflict with animation
-        layer.style.left = xMovement + 'px';
-        layer.style.top = yMovement + 'px';
+        layer.style.left = xMovement + '%';
+        layer.style.top = yMovement + '%';
     })
 }
