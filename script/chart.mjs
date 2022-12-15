97a43a24ba0130f80d5f93d61b3552de8bc3f3cc
Nico Barbieri <nicobarbieri.m@gmail.com>
2022-12-15 14:32:49 +0100

    update chart and add chart loading animation

--- script/chart.mjs
+++ script/chart.mjs
@@ -1,4 +1,5 @@
 let $swapGraph = document.getElementById('swap-graph').getContext("2d");
+let $loader = document.querySelector('.chart-loader');
 
 //empty object of data which will be filled randomly with simulateData()
 let chartData = {
@@ -8,7 +9,6 @@ let chartData = {
     'year': [],
 }
 
-let gradient = $swapGraph.createLinearGradient(0, 0, 0, 250);
 let delayed;
 let timeRange = 'day';
 
@@ -31,7 +31,7 @@ let valuesRange = (data) => {
 //randomly fill chartData
 let simulateData = (dataset, time = 'day') => {
     if (!dataset[time][0]) {
-        let startingPoint = 70;
+        let currentValue = 70;
         let numberOfValues;
         let timeMultiplier;
         switch (time) {
@@ -61,16 +61,14 @@ let simulateData = (dataset, time = 'day') => {
         }
 
         for (let i = 0; i < numberOfValues; i++) {
-            let value = dataset[timeRange][0]? dataset[timeRange][dataset[timeRange].length - 1].y : startingPoint;
-            dataset[timeRange].push({
-                // calculate current time in ms and then subtract timeMultiplier * (numberOfValues - i) to populate chartData
-                // first pushed value will be 24h before current hour
-                x: Date.now() - (timeMultiplier * (numberOfValues - i)), 
+            let value = dataset[timeRange][0]? dataset[timeRange][0].y : currentValue;
+            dataset[timeRange].unshift({
+                // calculate current time in ms and then subtract (timeMultiplier * i) to populate chartData
+                x: (Date.now() - (timeMultiplier * i)), 
                 y: randomSum(value, 6),
             })
         }
     }
-    
 }
 
 simulateData(chartData, 'day');
@@ -85,7 +83,8 @@ let redOrGreen = (array, opacity = 1) => {
 }
 
 //update gradient
-let updateGradient = (gradient, opacity1 = 1, opacity2 = 0) =>{
+let redOrGreenGradient = (opacity1 = 1, opacity2 = 0) =>{
+    let gradient = $swapGraph.createLinearGradient(0, 0, 0, 250);
     gradient.addColorStop(0, redOrGreen(chartData[timeRange], opacity1));
     gradient.addColorStop(1, redOrGreen(chartData[timeRange], opacity2));
     return gradient;
@@ -97,7 +96,8 @@ const data = {
     datasets: [{
         label: 'BNB/CAKE',
         data: chartData['day'],
-        backgroundColor: updateGradient(gradient, .5, 0),
+        fill: true,
+        backgroundColor: redOrGreenGradient(.5, 0),
         borderColor: redOrGreen(chartData[timeRange]),
         borderWidth: 2,
     }],
@@ -127,11 +127,10 @@ const config = {
                     display: false,
                 },
                 //y axis is hidden
-                display: false,
+                display: true,
             }
         },
         tension: .1, 
-        fill: true,
         elements: {
             point: {
                 radius: 0,
@@ -141,8 +140,14 @@ const config = {
         //delay animation on chart render 
         //https://www.chartjs.org/docs/latest/samples/animations/delay.html
         animation: {
+            onProgress: () => {
+                $loader.classList.remove('faded');
+                $loader.classList.add('loading');                
+            },
             onComplete: () => {
-              delayed = true;
+              //delayed = true;
+              $loader.classList.remove('loading');
+              $loader.classList.add('faded');
             },
             delay: (context) => {
               let delay = 0;
@@ -159,16 +164,24 @@ const config = {
 export const swapChart = new Chart($swapGraph, config)
 
 const $timeRangeButtons = document.querySelectorAll('.time-range-button');
-console.log($timeRangeButtons);
 
 $timeRangeButtons.forEach(button => {
     button.addEventListener('click', () =>{
-        timeRange = button.value;
-        simulateData(chartData, timeRange);
-        swapChart.data.datasets[0].data = chartData[timeRange];
-        swapChart.data.datasets[0].backgroundColor = updateGradient(gradient, .5, 0),
-        swapChart.data.datasets[0].borderColor = redOrGreen(chartData[timeRange]),
-        swapChart.update();
+        if (!button.classList.contains('active')) {
+            timeRange = button.value;
+            simulateData(chartData, timeRange);
+
+            //just to set last data value coherently
+            if (timeRange != 'day') {
+                chartData[timeRange][chartData[timeRange].length - 1].y = chartData['day'][chartData['day'].length - 1].y;
+            }
+
+            swapChart.data.datasets[0].data = chartData[timeRange];
+            swapChart.data.datasets[0].backgroundColor = redOrGreenGradient(.5, 0);
+            swapChart.data.datasets[0].borderColor = redOrGreen(chartData[timeRange]);
+
+            swapChart.update();
+        }
     })
 });
 
