51412104ed698f2ecc2a297be30ae06bc94607a4
Matteo Rania <matteo.rania2014@gmail.com>
2022-12-21 17:01:03 +0100

    added feature to sticky navbar

--- script/stickyNavbar.js
+++ script/stickyNavbar.js
@@ -1,11 +1,14 @@
+const navBar = document.getElementById("navbar")
+
 /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
 let prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-56px";
-  }
-  prevScrollpos = currentScrollPos;
+window.onscroll = function () {
+    let currentScrollPos = window.pageYOffset;
+    if (prevScrollpos >= currentScrollPos) {
+        navBar.style.top = "0";
+    }
+    else if (currentScrollPos > 70) {
+        navBar.style.top = "-56px";
+    }
+    prevScrollpos = currentScrollPos;
 }
