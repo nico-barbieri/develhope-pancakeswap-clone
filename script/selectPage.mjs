6c52ab302aabd049dccd96b64161435cb88abed5
Nico Barbieri <nicobarbieri.m@gmail.com>
2022-12-23 04:37:47 +0100

    update liquidity responsive and bug fixes

--- script/selectPage.mjs
+++ script/selectPage.mjs
@@ -4,14 +4,15 @@ const selectPage = () => {
     $buttons.forEach(button =>{
         button.addEventListener('click', () =>{
             if (button.dataset.section == null) {
-                console.log('here');
                 return; //do not show empy windows
             }
             $sections.forEach(sec=>{
+                sec.classList.remove('show-window');
                 sec.style.display= "none";
             })
             const section = document.getElementById(button.dataset.section);
             section.style.display = "flex";
+            section.classList.add('show-window');
         })
     })
 }
