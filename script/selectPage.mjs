71db904657f7f50f8311404705c8da22c6bbb053
Nico Barbieri <nicobarbieri.m@gmail.com>
2022-12-23 03:15:00 +0100

    add select page script on trade

--- script/selectPage.mjs
+++ script/selectPage.mjs
@@ -1,14 +1,17 @@
-import { buttons } from "./buttons.mjs";
-
-const $target = document.getElementById('main-window');
 const $buttons = document.querySelectorAll('.small-menu-button');
-
+const $sections = document.querySelectorAll('.trade-section');
 const selectPage = () => {
     $buttons.forEach(button =>{
-        button.addEventListener('click', async () =>{
-            const section = button.dataset.section;
-            const html = await (await fetch(`./trade-components/${section}.html`)).text();
-            $target.innerHTML = html;
+        button.addEventListener('click', () =>{
+            if (button.dataset.section == null) {
+                console.log('here');
+                return; //do not show empy windows
+            }
+            $sections.forEach(sec=>{
+                sec.style.display= "none";
+            })
+            const section = document.getElementById(button.dataset.section);
+            section.style.display = "flex";
         })
     })
 }
